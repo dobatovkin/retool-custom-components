19c703c44e6831042a268bde4e46b3b34a0bc472
dobatovkin <yahor.dabatoukin@mapbox.com>
2023-11-17 18:43:53 +0300

    feat: add basemap selector

--- mapbox-map/src/map-component.js
+++ mapbox-map/src/map-component.js
@@ -1,20 +1,44 @@
 import React, { useRef, useEffect, useState } from "react";
 
-import { Box, Button, Divider, TextField, Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  Divider,
+  TextField,
+  Typography,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  SelectChangeEvent,
+} from "@mui/material";
 
 import mapboxgl from "mapbox-gl";
 
 /**
- * @type MapComponentModel
- * @property
+ * Extended Mapbox GL JS Layer with component-specific properties,
+ * @typedef {object} MapComponentLayer
+ * @extends import("mapbox-gl").Layer
+ * @property {boolean} itemDependant whether to rerender layer on `itemId` change
  */
 
 /**
- *
- * @param {*} param0
- * @returns
+ * Extended Mapbox GL JS Marker with component-specific properties.
+ * @typedef {object} MapComponentMarker
+ * @extends import("mapbox-gl").Layer
+ * @property {boolean} itemDependant whether to rerender marker on `itemId` change
  */
+
+/**
+ * @typedef {object} MapComponentModel
+ * @property {string} mapboxAccessToken Access token for Mapbox GL JS
+ * @property {string} itemId ID of the current selected item in Retool. Change causes item-specific layers and markers to rerender.
+ * @property {MapComponentLayer[]} layers Array of layers to display.
+ * @property {import("mapbox-gl").MarkerOptions[]} markers Array of markers to display.
+ */
+
 const MapComponent = ({ triggerQuery, model, modelUpdate }) => {
+  const mapBasemapRef = useRef(null);
   const mapContainerRef = useRef(null);
   const mapRef = useRef(null);
 
@@ -39,7 +63,7 @@ const MapComponent = ({ triggerQuery, model, modelUpdate }) => {
   }, [mapContainerRef]);
 
   useEffect(() => {
-    // Check if the map is initialized
+    // check if the map is initialized and itemId is accessible
     if (mapRef.current && model.itemId) {
       // Fit the map to the bounding box
       mapRef.current.flyTo({
@@ -48,9 +72,32 @@ const MapComponent = ({ triggerQuery, model, modelUpdate }) => {
       });
     }
   }, [model]);
+  const [basemapId, setBasemapId] = React.useState("");
+
+  const handleBasemapIdChange = (event) => {
+    setBasemapId(event.target.value);
+  };
   return (
     <div>
       <div id="map" ref={mapContainerRef}></div>
+
+      <Box sx={{ maxWidth: 120, margin: "10px", bgcolor: "white" }}>
+        <FormControl fullWidth sx={{ margin: "5px" }}>
+          <InputLabel id="basemap-select-label">Basemap</InputLabel>
+          <Select
+            labelId="basemap-select-label"
+            id="basemap-select"
+            value={basemapId}
+            label="Basemap"
+            onChange={handleBasemapIdChange}
+          >
+            {/* basemapList.map((basemapItem) => {}) */}
+            <MenuItem value={10}>Ten</MenuItem>
+            <MenuItem value={20}>Twenty</MenuItem>
+            <MenuItem value={30}>Thirty</MenuItem>
+          </Select>
+        </FormControl>
+      </Box>
     </div>
   );
 };
